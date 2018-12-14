36576741519c09ef30c0d166553d53dd6969831f
lijiahangmax <42642056+lijiahangmax@users.noreply.github.com>
2018-12-14 14:05:04 +0800

    项目创建

---
+++ src/main/java/com/sysmin/SysminApplication.java
@@ -0,0 +1,14 @@
+package com.sysmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysminApplication.class, args);
+    }
+
+}
+
